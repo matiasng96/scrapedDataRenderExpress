f2c43f78d55c333e59be7f51697de0135615ef17
Matías Gallego <matiasngallego@gmail.com>
2021-02-07 13:55:02 -0300

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const uri = 'mongodb+srv://matiasng:GnXBcKvAIobg2Zv9@cluster0.4i9ha.mongodb.net/test';
+const mongodb_uri = 'mongodb+srv://matiasng:GnXBcKvAIobg2Zv9@cluster0.4i9ha.mongodb.net/test';
 const mongoose = require('mongoose');
 const leagueteam = require('./models/leagueteam');
 //Mongoose conection
 mongoose
-	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
 		console.log('Conectado!');
 	})
